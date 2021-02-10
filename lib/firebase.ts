a783f9df7f8ca01d336695497a72e1e755bd8fed
Aubrey Mills <aubreymills@satisoftware.com>
2021-02-10 16:29:03 -0500

    Initial commit with TypeScript and Firebase

---
+++ lib/firebase.ts
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBiOajoA_8fRfSrgymd9pEWVVEJNGeUIxs",
+  authDomain: "aubrey-blog.firebaseapp.com",
+  projectId: "aubrey-blog",
+  storageBucket: "aubrey-blog.appspot.com",
+  messagingSenderId: "74007278838",
+  appId: "1:74007278838:web:475f9a9e16beed022328d7",
+  measurementId: "G-QSNH8T7MCS"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();

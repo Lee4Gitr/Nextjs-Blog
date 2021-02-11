cedfd2bfea4b5f8b8827cf44a2cf09b6e12bc394
Aubrey Mills <aubreymills@satisoftware.com>
2021-02-10 21:01:08 -0500

    Sign in working and refactored

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link';
+import {UserContext} from "../lib/context";
+import {useContext} from "react";
 
 export default function Navbar() {
-  // @ts-ignore
-  // const {user, username} = {};
-  const user = true;
-  const username = true;
+
+  const { user, username } = useContext(UserContext)
+
   return (
     <nav className="navbar">
       <ul>
